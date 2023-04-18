fdada07e668668407b951d55e941c07e7bd03dfc
PrathmeshVyas <prathmeshvyas2002@gmail.com>
2023-04-18 19:31:19 +0530

    vendor created

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import UserForm
-from .models import User
+from accounts.forms import UserForm
+from vendor.forms import VendorForm
+from accounts.models import User, UserProfile
 from django.contrib import messages
+
 # Create your views here.
+
 
 def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             # create user using create user method
-            first_name=form.cleaned_data['first_name']
-            last_name=form.cleaned_data['last_name']
-            username=form.cleaned_data['username']
-            email=form.cleaned_data['email']
-            password=form.cleaned_data['password']
-            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(
+                first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
             user.save()
             # create user using form
@@ -24,7 +28,8 @@ def registerUser(request):
             # user.set_password(password)
             # user.role = User.CUSTOMER
             # user.save()
-            messages.success(request, 'your account has been register successfully')
+            messages.success(
+                request, 'your account has been register successfully')
             return redirect('registerUser')
         else:
             print('invalid')
@@ -32,5 +37,39 @@ def registerUser(request):
     else:
         form = UserForm()
 
-    context = {'form':form}
-    return render(request, 'accounts/registerUser.html', context) 
+    context = {'form': form}
+    return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+    if request.method == 'POST':
+        # store data and create User
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        print(v_form.data)
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'your account has been register successfully! please wait for approval.')
+            return redirect('registerVendor')
+        else:
+            print('invalid form')
+            print(v_form.errors)
+    else:   
+        form = UserForm()
+        v_form = VendorForm()
+
+    context = {'form': form, 'v_form': v_form}
+
+    return render(request, 'accounts/registerVendor.html', context)
