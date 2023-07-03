e29c6b28e86b19e1b2fd31a05b7d3ab44dea9f65
PrathmeshVyas <prathmeshvyas2002@gmail.com>
2023-07-03 16:16:35 +0530

    get user's current location and organize home page to display nearby restaurants

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -10,4 +10,4 @@ def get_vendor(request):
     return dict(vendor=vendor)
 
 def get_google_api(request):
-    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
+    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY }
